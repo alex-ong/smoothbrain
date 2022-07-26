3cd85c65ed73572df0185945d9a4704af0cc8054
alexo <alex-ong>
2022-07-26 14:18:33 +1000

    new: Super smooth working

--- Assets/Spinners.cs
+++ Assets/Spinners.cs
@@ -21,17 +21,35 @@ public class Spinners : MonoBehaviour
         
     }
 
+    const int HOUR_TO_SIXTY = 5;
+    const float MINUTE_PER_HOUR = 60f;
+    const float SECOND_PER_MINUTE = 60f;
+    const float SECOND_PER_HOUR = MINUTE_PER_HOUR * SECOND_PER_MINUTE; //3600f
+    const float MILLIS_PER_SECOND = 1000f;
+    const float MILLIS_PER_MINUTE = MILLIS_PER_SECOND*SECOND_PER_MINUTE;
+    const float MILLIS_PER_HOUR = MILLIS_PER_SECOND * SECOND_PER_HOUR; //3600000f
+
+    /// <summary>
+    /// Returns value from [0,60)
+    /// </summary>
+    /// <returns></returns>
     private float GetValue()
     {
         var tr = timeReference.GetTime();
         switch (timeType)
         {
             case TimeRef.Hour:
-                return tr.Hour % 12 * 5 + 1/60f * tr.Minute + 1/3600f * tr.Second + 1/3600000f * tr.Millisecond;
+                return HOUR_TO_SIXTY * ((tr.Hour) 
+                    + (tr.Minute/MINUTE_PER_HOUR) 
+                    + (tr.Second/SECOND_PER_HOUR) 
+                    + (tr.Millisecond/MILLIS_PER_HOUR));
             case TimeRef.Minute:
-                return tr.Minute + 1/60f * tr.Second + 1/60000f * tr.Millisecond;
+                return tr.Minute 
+                    + (tr.Second/SECOND_PER_MINUTE)
+                    + (tr.Millisecond/MILLIS_PER_MINUTE);
             case TimeRef.Second:
-                return tr.Second + (1/1000f * tr.Millisecond);
+                return tr.Second 
+                    + (tr.Millisecond/MILLIS_PER_SECOND);
             default:
                 return tr.Second;
         }
